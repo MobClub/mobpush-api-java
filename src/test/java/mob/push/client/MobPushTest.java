ec34dce7ea30780d222e0994decaf4ebb7359ea9
moblarry <lipf@yoozoo.com>
2020-04-27 18:28:23 +0800

    update

---
+++ src/test/java/mob/push/client/MobPushTest.java
@@ -0,0 +1,21 @@
+/**
+ * Project Name:mobpush-api-java-client
+ * File Name:MobPushTest.java
+ * Package Name:org.mob.push.client
+ * Date: 2018年2月7日
+ * Time: 下午12:31:00
+ *
+*/
+
+package mob.push.client;
+ 
+/**
+ * ClassName:MobPushTest
+ * PushClient TEST
+ * Date: 2018年2月7日
+ * Time: 下午12:31:00 
+ */
+public class MobPushTest {
+	 
+}
+
